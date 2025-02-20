f6882cc7fbe0a6ed6a456863bc293bbe05cdcf6b
uthso297 <uthso119464@gmail.com>
2025-02-21 01:58:14 +0600

    implemented edit task

--- src/Pages/WorkSpace/Workspace/Workspace.jsx
+++ src/Pages/WorkSpace/Workspace/Workspace.jsx
@@ -4,9 +4,13 @@ import { useEffect, useState } from 'react';
 
 const Workspace = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [isEditModalOpen, setIsEditModalOpen] = useState(false);
     const [title, setTitle] = useState('');
     const [description, setDescription] = useState('');
     const [tasks, setTasks] = useState([]);
+    const [currentTaskId, setCurrentTaskId] = useState(null);
+    const [errorTittle, setErrorTittle] = useState('');
+    const [errorDescription, setErrorDescription] = useState('');
 
     useEffect(() => {
         fetch('http://localhost:5000/task')
@@ -27,11 +31,22 @@ const Workspace = () => {
         setIsModalOpen(false);
         setTitle('');
         setDescription('');
+        setErrorTittle('')
+        setErrorDescription('')
     };
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const task = { title, description }
+        if (title.length > 50) {
+            setErrorTittle("title should not exceed 50 characters")
+            return;
+        }
+        if (description.length > 200) {
+            setErrorDescription("description should not exceed 200 characters")
+            return;
+        }
+
+        const task = { title, description, category: 'To-Do' }
         fetch('http://localhost:5000/task', {
             method: 'POST',
             headers: {
@@ -42,6 +57,47 @@ const Workspace = () => {
         handleCloseModal();
     };
 
+    const handleEditSubmit = (e) => {
+        e.preventDefault();
+        if (title.length > 50) {
+            setErrorTittle("title should not exceed 50 characters")
+            return;
+        }
+        if (description.length > 200) {
+            setErrorDescription("description should not exceed 200 characters")
+            return;
+        }
+        const updatedTask = { title, description };
+        fetch(`http://localhost:5000/task/${currentTaskId}`, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedTask)
+        })
+            .then(() => {
+                handleCloseEditModal();
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+        // console.log(updatedTask);
+    };
+
+    const handleOpenEditModal = (task) => {
+        setIsEditModalOpen(true);
+        setTitle(task.title);
+        setDescription(task.description);
+        setCurrentTaskId(task._id);
+    };
+
+    const handleCloseEditModal = () => {
+        setIsEditModalOpen(false);
+        setTitle('');
+        setDescription('');
+        setErrorTittle('')
+        setErrorDescription('')
+    };
 
 
     const handleDelete = (id) => {
@@ -62,7 +118,7 @@ const Workspace = () => {
                 height: '100vh',
             }}
         >
-            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[200px] p-4 bg-[#1d2025] opacity-70 rounded-lg overflow-y-auto'>
+            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[600px] p-4 bg-[#1d2025] opacity-70 rounded-lg overflow-y-auto'>
                 <h1 className='text-center text-2xl text-white font-bold'>To Do</h1>
                 <div className='w-full mx-auto h-1 rounded-lg mt-5 bg-white' />
                 <div className='mt-3 space-y-2'>
@@ -79,7 +135,7 @@ const Workspace = () => {
                             <h1 className='text-2xl text-white font-bold'>{task.title}</h1>
                             <p className='text-white'>{task.description}</p>
                             <div className='flex items-center gap-3'>
-                                <button className='bg-[#9fabdc] px-3 py-2 rounded-lg cursor-pointer'>Edit</button>
+                                <button onClick={() => handleOpenEditModal(task)} className='bg-[#9fabdc] px-3 py-2 rounded-lg cursor-pointer'>Edit</button>
                                 <button onClick={() => handleDelete(task._id)} className='bg-[#9fabdc] px-3 py-2 rounded-lg cursor-pointer'>Delete</button>
                             </div>
                         </div>)
@@ -87,7 +143,7 @@ const Workspace = () => {
                 </div>
             </div>
 
-            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[200px] p-4 bg-[#1d2025] opacity-70 rounded-lg'>
+            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[400px] p-4 bg-[#1d2025] opacity-70 rounded-lg'>
                 <h1 className='text-center text-2xl text-white font-bold'>In Progress</h1>
                 <div className='w-full mx-auto h-1 rounded-lg mt-5 bg-white' />
                 <div>
@@ -95,7 +151,7 @@ const Workspace = () => {
                 </div>
             </div>
 
-            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[200px] p-4 bg-[#1d2025] opacity-70 rounded-lg'>
+            <div className='lg:w-[30%] lg:h-[60vh] w-full h-[400px] p-4 bg-[#1d2025] opacity-70 rounded-lg'>
                 <h1 className='text-center text-2xl text-white font-bold'>Done</h1>
                 <div className='w-full mx-auto h-1 rounded-lg mt-5 bg-white' />
                 <div>
@@ -120,6 +176,7 @@ const Workspace = () => {
                                     required
                                 />
                             </div>
+                            {errorTittle && <p className='text-red-600'>{errorTittle}</p>}
                             <div className="mb-4">
                                 <label htmlFor="description" className="block text-lg font-medium mb-2">Description</label>
                                 <textarea
@@ -130,6 +187,7 @@ const Workspace = () => {
                                     required
                                 />
                             </div>
+                            {errorDescription && <p className='text-red-600'>{errorDescription}</p>}
                             <div className="flex justify-between">
                                 <button
                                     type="submit"
@@ -149,6 +207,60 @@ const Workspace = () => {
                     </div>
                 </div>
             )}
+
+            {/* modal for editing task */}
+
+            {isEditModalOpen && (
+                <div className="fixed inset-0 flex items-center justify-center">
+                    <div className="bg-white p-6 rounded-lg w-11/12 sm:w-96 md:w-[50%] lg:w-[30%]">
+                        <h2 className="text-2xl font-bold mb-4">Edit Task</h2>
+                        <form onSubmit={handleEditSubmit}>
+                            <div className="mb-4">
+                                <label htmlFor="title" className="block text-lg font-medium mb-2">Title</label>
+                                <input
+                                    type="text"
+                                    id="title"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    className="w-full p-2 border rounded-lg"
+                                    required
+                                />
+                            </div>
+                            {errorTittle && <p className='text-red-600'>{errorTittle}</p>}
+
+                            <div className="mb-4">
+                                <label htmlFor="description" className="block text-lg font-medium mb-2">Description</label>
+                                <textarea
+                                    id="description"
+                                    value={description}
+                                    onChange={(e) => setDescription(e.target.value)}
+                                    className="w-full p-2 border rounded-lg"
+                                    required
+                                />
+                            </div>
+                            {errorDescription && <p className='text-red-600'>{errorDescription}</p>}
+
+                            <div className="flex justify-between">
+                                <button
+                                    type="submit"
+                                    className="px-4 py-2 bg-green-500 text-white rounded-lg"
+                                >
+                                    Update
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={handleCloseEditModal}
+                                    className="px-4 py-2 bg-red-500 text-white rounded-lg"
+                                >
+                                    Cancel
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            )}
+
+
         </div>
     );
 };
